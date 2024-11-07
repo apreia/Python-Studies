d75280ccd3c791d2ddcd62698c0113db1390770d
apreia <joao.apreia@gmail.com>
2024-11-07 11:39:26 -0300

    Create Desafio 03.py
    Adicionado o Desafio 03 da aula 4

---
+++ Mundo 1/Aula 4/Desafio 03.py
@@ -0,0 +1,5 @@
+primeiro_numero = int(input("Primeiro número: "))
+segundo_numero = int(input("Segundo número: "))
+soma = (primeiro_numero+segundo_numero)
+
+print("A soma de",primeiro_numero,"+",segundo_numero,"é =",soma)
